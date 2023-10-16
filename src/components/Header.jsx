25d8693824fb7dd0c26c1f8b88ebaf444b8cbbdd
Mehman Aliyev <mehman.va@gmail.com>
2023-10-16 21:28:28 +0400

    header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='header'>
+        <ul>
+            <li>
+                <a href="">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    Products
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    About
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    Contact
+                </a>
+            </li>
+        </ul>
+    </header>
+  )
+}
+
+export default Header
